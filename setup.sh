52178e8520b93273b8a5190e933a2f32025d100a
Kevin Dyer <kevin.m.dyer@gmail.com>
2018-03-24 12:11:28 -0600

    feat: updated setup script

--- setup.sh
+++ setup.sh
@@ -4,33 +4,23 @@ cp .gitconfig ~/.
 cp .vimrc ~/.
 
 # Node
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+npm install -g \
+  yarn \
+  firebase-tools \
+  polymer-cli \
+  commitizen cz-conventional-changelog
 
-# NPM Permissions
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-
-if [[ "0" == "$(grep -c "# NPM" ~/.bashrc)" ]]; then
-  echo "# NPM" >> ~/.bashrc
-  echo "export PATH=~/.npm-global/bin:\$PATH" >> ~/.bashrc
-  echo "" >> ~/.bashrc
-fi
-npm install -g npm
-npm install -g yarn firebase-tools polymer-cli
+echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
 
 # Google Cloud Platform
-# Create an environment variable for the correct distribution
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-
-# Add the Cloud SDK distribution URI as a package source
 echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
-# Import the Google Cloud Platform public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-# Update the package list and install the Cloud SDK
-sudo apt-get update && sudo apt-get install google-cloud-sdk
+sudo apt-get update
+sudo apt-get install -y \
+  google-cloud-sdk \
+  build-essential
 
 # PS1='[\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\]$(__git_ps1 " (%s)")]\$ '
